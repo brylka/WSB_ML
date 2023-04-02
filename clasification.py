a5718d0f128f29aba7049df0cfb614057290079b
Bartosz Bryniarski <brylka@jelenia.com>
2023-04-02 07:54:53 +0200

    Initial commit

---
+++ clasification.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.datasets.mnist import load_data
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense, Dropout
+
+np.set_printoptions(precision=12, suppress=True, linewidth=120)
+
+(X_train, y_train), (X_test, y_test) = load_data()
+
+print(f'X_train shape: {X_train.shape}')
+print(f'y_train shape: {y_train.shape}')
+print(f'X_test shape: {X_test.shape}')
+print(f'y_test shape: {y_test.shape}')
+
+#print(y_train[0])
+
+# Tworzenie modelu
+model = Sequential()
+model.add(Flatten(input_shape=(28, 28)))
+model.add(Dense(units=128, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(units=10, activation='softmax'))
+
+# Kompilacja modelu
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+print(model.summary())
+
+# Trenowanie modelu
+history = model.fit(X_train, y_train, epochs=15)
+
+model.evaluate(X_test, y_test, verbose=2)
+
+y_pred = model.predict(X_test)
+
+y_pred_classes = np.argmax(y_pred, axis=1)
+
+print(y_pred_classes)
+
+pred = pd.concat([pd.DataFrame(y_test, columns=['y_test']), pd.DataFrame(y_pred_classes, columns=['y_pred'])], axis=1)
+print(pred.head(10))
+
+misclassified = pred[pred['y_test'] != pred['y_pred']]
+
+plt.figure(figsize=(13,13))
+
+for i, j in zip(range(1,12), misclassified.index[:10]):
+    plt.subplot(1, 10, i)
+    plt.axis('off')
+    plt.imshow(X_test[j], cmap='gray_r')
+    plt.title('y_test: ' + str(y_test[j]) + '\n' + 'y_pred: ' + str(y_pred_classes[j]), color='black', fontsize=12)
+plt.show()
---
+++ regresion.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.random import set_seed
+
+set_seed(0)
+
+# komponenty sieci
+model = Sequential()
+#                       linear, relu, sigmoid, tanh, softmax
+model.add(Dense(4, input_shape=[1], activation="linear"))
+model.add(Dense(2, activation="linear"))
+model.add(Dense(1))
+
+#kompilacja modelu
+model.compile(optimizer="rmsprop", loss="mse", metrics=["mae"])
+df = pd.read_csv("https://vesemir.wiedzmin.net/extras/f-c.csv", usecols=[1,2])
+
+print(df.head())
+
+plt.scatter(df.F, df.C)
+#plt.show()
+
+# uczenie sieci
+result = model.fit(df.F, df.C, epochs=1500, verbose=0)
+
+print(result.history.keys())
+
+df1 = pd.DataFrame(result.history)
+print(df1.head(500))
+
+#df1.plot()
+#plt.show()
+
+y_pred = model.predict(df.F)
+
+plt.scatter(df.F, df.C)
+
+plt.plot(df.F, y_pred, c='r')
+
+plt.show()
